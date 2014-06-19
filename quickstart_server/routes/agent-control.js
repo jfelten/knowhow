cc5e30c03da731e0c8f2dbf7d485855867168703
John Felten <john_felten@yahoo.com>
2014-06-19 12:17:23 -0700

    added event handling and improved error handling

--- quickstart_server/routes/agent-control.js
+++ quickstart_server/routes/agent-control.js
@@ -1,10 +1,14 @@
 var async = require('async');
-var client = require('scp2')
+var client = require('scp2');
 var zlib = require('zlib');
 var fstream = require('fstream');
 var tar = require('tar');
 var Connection = require('ssh2');
+var EventEmitter = require('events').EventEmitter;
+var eventEmitter = new EventEmitter();
 //deliver the agent files
+
+
 
 function install(agent, commands) {
 	var execCommands = new Array(commands.length);
@@ -43,6 +47,8 @@ function install(agent, commands) {
 		    	    });
 		    	    conn.on('close', function(had_error) {
 		    	      console.log('Connection :: close');
+		    	      
+		    	      eventEmitter.emit('executed:',agent, comm);
 		    	      callback();
 		    	    }); 
 		    	    //.stderr.on('data', function(data) {
@@ -54,6 +60,9 @@ function install(agent, commands) {
 			  username: agent.user,
 			  password: agent.password
 			});
+	    	conn.on('error', function(er) {
+	    		console.log('unable to connect to: '+agent.host,er.message);
+	    	});
 	    	
 	    	
 		}.bind( {'cmd': command, 'idx': index});
@@ -76,26 +85,38 @@ exports.addAgent = function(agent) {
 	.pipe(tar.Pack()) /* Convert the directory to a .tar file */
 	.pipe(zlib.Gzip()) /* Compress the .tar file */
 	.pipe(fstream.Writer({ 'path': agent_archive_name }).on("close", function () {
+		eventEmitter.emit('package-complete', agent);
 		console.log('delivering agent files to: '+agent.host);
 		client.scp(__dirname+"/../"+agent_archive_name, {
 		    host: agent.host,
 		    username: agent.user,
 		    password: agent.password,
 		    path: '/home/'+agent.user
-		}, function(err) {console.log(err);})
-
-		client.on('close',function () {
+		}, function(err) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+			
+			eventEmitter.emit('transfer-complete', agent);
 			console.log('transfer complete');
 			
 			//start the agent
 			console.log('starting agent on: '+agent.host);
-			  commands=['tar xzf '+agent_archive_name,
+			commands=['tar xzf '+agent_archive_name,
 			            'tar xzf quickstart_agent/node*.tar.gz',
 			            'nohup node*/bin/node quickstart_agent/agent.js'
-			  ];
-	         install(agent, commands);
-			
+			];
+	        install(agent, commands);
+		});
+
+		client.on('close',function (err) {
+	
 		    });
+		client.on('error',function (err) {
+			eventEmitter.emit('agent-error', agent, err.syscall+" "+err.code);
+			console.log('error delivering agent: ', err);
+		});
 	}
 			
 	));
