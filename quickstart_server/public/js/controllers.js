d95ce38afa23d297d4296fad8e0da33827ab6684
John Felten <john_felten@yahoo.com>
2014-07-01 23:54:23 -0700

    added bootstrap

--- quickstart_server/public/js/controllers.js
+++ quickstart_server/public/js/controllers.js
@@ -84,7 +84,16 @@ angular.module('myApp.controllers', []).
         });
     };
 	  
+    $scope.tabs = [
+                   { title:'Dynamic Title 1', content:'Dynamic content 1' },
+                   { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+                 ];
 
+     $scope.alertMe = function() {
+       setTimeout(function() {
+         alert('You\'ve selected the alert tab!');
+       });
+     };
   }).
   controller('MyCtrl2', function ($scope) {
     // write Ctrl here
