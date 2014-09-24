990688f7aca68f7d7677f94a50508d284de35469
John Felten <john_felten@yahoo.com>
2014-09-24 09:26:04 -0700

    fix typo

--- quickstart_server/routes/execution-control.js
+++ quickstart_server/routes/execution-control.js
@@ -83,7 +83,7 @@ cancelJob = function(agentId, jobId, callback ) {
 		//	currentJobs[agentId].fileSocket.close();
 		//}
 		//if (currentJobs[agentId][jobId].eventSocket != undefined) {
-		//	currentJobs[agentId][jobId].eventSocket.close();
+		//	currentJlobs[agentId][jobId].eventSocket.close();
 		//}
 		for (uploadFile in currentJobs[agentId][jobId].fileProgress) {
 			if (currentJobs[agentId][jobId].fileProgress[uploadFile].readStream != null) {
@@ -283,7 +283,7 @@ function uploadFiles(agent,job, callback) {
 		//	currentJobs[agentId].eventSocket.emit('job-cancel',jobId);
 		//	cancelJob(agent, job);
 		//	callback(new Error("Problem starting file upload"));
-			callback(new Error("unable to start upload for: "+fileControl.getFilePath(files[uploadFile].source));
+			callback(new Error("unable to start upload for: "+fileControl.getFilePath(files[uploadFile].source)));
 			return;
 			break;
 		}
