80b667702aa3fb7fc0d6653a6506f5584f1f6391
John Felten <john_felten@yahoo.com>
2014-10-31 16:35:20 -0700

    make process log to the directory it was start from

--- scripts/startKHAgent.sh
+++ scripts/startKHAgent.sh
@@ -4,5 +4,5 @@ var path = require('path')
 , fs = require('fs')
 ,agent  = path.join(path.dirname(fs.realpathSync(__filename)), '../node_modules/knowhow_agent/agent.js');
 
-console.log("executing: "+server);
+console.log("executing: "+agent);
 require(agent);
