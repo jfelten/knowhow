7bc043b31d45b1be10b2d7cb73fe49e88ee76ec9
John Felten <john_felten@yahoo.com>
2014-11-02 18:40:18 -0800

    add start scripts for the rpm

---
+++ scripts/startKHAgent_inc.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+../node-v*/bin/node ../node_modules/knowhow_agent/agent.js &
+../node-v*/bin/node ../node_modules/knowhow_server/server.js
---
+++ scripts/startKHServer_inc.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+../node-v*/bin/node ../node_modules/knowhow_agent/agent.js
