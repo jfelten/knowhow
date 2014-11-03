0155e6d4fc523e72e7a3d1e4de2cc6ad5b335b8c
John Felten <john_felten@yahoo.com>
2014-11-02 22:53:43 -0800

    corected executable path locations

--- scripts/startKHAgent_inc.sh
+++ scripts/startKHAgent_inc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/lib/node_modules/knowhow/node-*/bin/node ../node_modules/knowhow_agent/agent.js
+/usr/lib/node_modules/knowhow/node-*/bin/node /usr/lib/node_modules/knowhow/node_modules/knowhow_agent/agent.js
--- scripts/startKHServer_inc.sh
+++ scripts/startKHServer_inc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/lib/node_modules/knowhow/node-*/bin/node ../node_modules/knowhow_agent/agent.js &
-/usr/lib/node_modules/knowhow/node-*/bin/node ../node_modules/knowhow_server/server.js
+/usr/lib/node_modules/knowhow/node-*/bin/node /usr/lib/node_modules/knowhow/node_modules/knowhow_agent/agent.js &
+/usr/lib/node_modules/knowhow/node-*/bin/node /usr/lib/node_modules/knowhow/node_modules/knowhow_server/server.js
