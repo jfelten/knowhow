e34dd795dca68cdc8393821d6f568e305e8f31b2
knowhow <john@zenzic.com>
2015-04-30 17:02:30 -0700

    correct path

--- scripts/KHCommand.sh
+++ scripts/KHCommand.sh
@@ -2,6 +2,6 @@
 
 var path = require('path')
 , fs = require('fs')
-,server  = path.join(path.dirname(fs.realpathSync(__filename)), '../node_modules/knowhow-api/commandTool.js')
+,server  = path.join(path.dirname(fs.realpathSync(__filename)), '../node_modules/knowhow-api/kh-api/commandTool.js')
 
 require(server);
